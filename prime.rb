ec49a8f9df7625550adef1d2b6c44779cd5c6784
Zoe Bai <s.xue.bai@gmail.com>
2019-02-09 15:54:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -8,3 +8,5 @@ def prime?(numbers)
     (numbers % number == 0)? false : true
   end
 end
+
+# logic: We are checking a range (from 2 to the argument number - 1), in which if the argument number is not divisible by all of them, then it is prime number, otherwise not 

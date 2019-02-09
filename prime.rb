cd09f875373c265b929a1986718a11421fff0d80
Zoe Bai <s.xue.bai@gmail.com>
2019-02-09 15:52:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+
+def prime?(numbers)
+  while numbers <= 1
+    return false
+  end
+  
+  (2..numbers-1).all? do |number|
+    (numbers % number == 0)? false : true
+  end
+end

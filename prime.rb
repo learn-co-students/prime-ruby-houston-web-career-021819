04e7343a5011862788d739a6441fc91cfca0da41
zepher2211 <zepher2211@gmail.com>
2019-02-18 15:36:05 -0600

    methods programmed

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  (2..).each do |x|
+ if num <= 1
+    return false
+    break
+ elsif num == x
+    return true
+    break
+  elsif num % x == 0 && num != x
+    return false
+    break
+  end
+ end
+end
